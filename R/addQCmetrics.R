c6f9722d123789cf5595066481328ddef38f7a3c
Micah Fletcher <micahpfletcher@gmail.com>
2023-04-21 12:59:40 -0400

    added addQCmetrics (port of old calculateQCmetrics)

---
+++ R/addQCmetrics.R
@@ -0,0 +1,31 @@
+#' Add QC metrics
+#'
+#' Calculates qc metrics and reformats Seurat object metadata to be more readable
+#'
+#' @param obj Seurat object
+#' @param mito.pattern pattern defining mitochondiral genes
+#' @param ribo.pattern pattern defining ribosomal genes
+#'
+#' @return Seurat object with modified metadata
+#'
+#' @export
+addQCmetrics <-
+  function(obj, mito.pattern="^MT", ribo.pattern="^RP[SL]") {
+    # Add number of genes per UMI for each cell to metadata
+    obj$log10GenesPerUMI <- log10(obj$nFeature_RNA)/log10(obj$nCount_RNA)
+    # Mitochondrial ratio
+    obj$mitoRatio <- PercentageFeatureSet(object=obj, pattern=mito.pattern)/100
+    # Ribosomal ratio
+    obj$riboRatio <- PercentageFeatureSet(object=obj, pattern=ribo.pattern)/100
+    # Create metadata dataframe
+    metadata <- obj@meta.data
+    # Add cell IDs to metadata
+    metadata$cells <- rownames(metadata)
+    # Rename columns to be more readable/intuitive
+    metadata <- metadata %>%
+      dplyr::rename(capID = orig.ident,
+                    nUMI = nCount_RNA,
+                    nGene = nFeature_RNA)
+    obj@meta.data <- metadata
+    return(obj)
+  }
