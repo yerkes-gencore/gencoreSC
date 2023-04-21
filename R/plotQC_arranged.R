60cc175899cdfb6adb2a42f1b746bc31ff77cfea
Micah Fletcher <micahpfletcher@gmail.com>
2023-04-21 16:52:35 -0400

    fixed typo in plotQC_arranges documentation

--- R/plotQC_arranged.R
+++ R/plotQC_arranged.R
@@ -7,7 +7,7 @@
 #' @param split_by metadata column to split ridge plots by (i.e. usually capture ID or sample ID)
 #' @param color_by metric with to use for scale_color_gradient (i.e. "mitoRatio" or "riboRatio" or any other metric with limits=c(0,1))
 #' @param cutoffs named list of cutoff values used in 'addQCmetrics()' for the given filtName
-#' @param title main title of
+#' @param title main title
 #'
 #' @return an object of class ggarrange, which is a ggplot or a list of ggplot.
 #'
