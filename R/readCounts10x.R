db65aa010ead6eead20c85f97d9d8074887c57ca
Derrik Gratz <derrik.gratz@gmail.com>
2023-05-08 15:41:48 -0400

    Simplify and separate seurat object creation from primary filtering
    This allows compatibility with SoupX workflow and generalizes this function more for all multi-run datasets

--- R/readCounts10x.R
+++ R/readCounts10x.R
@@ -3,65 +3,37 @@
 #' Reads in 10X counts and metadata and returns Seurat obj
 #'
 #' @param filepath path to UMI count matrix (can be raw or unfiltered)
-#' @param min.cells minimum number of cells per feature for RNA assay
-#' @param min.features minimum number of features per cell for RNA assay
 #' @param capID capture ID label, assigned to 'project' in seurat object metadata
+#' @inheritParams SeuratObject
 #'
-#' @return Seurat object with the number of cells and features removed stored in 'nRemoved' in 'misc' slot
+#' @return An object of class Seurat
 #'
-#' @note
-#' To do:
-#'   - add working example
-#'   - add option to not remove multiplets and undetermined droplets
-#'   - currently only accepts RNA or RNA + ADT assay, add ability to handle arbitrary additional assays (e.g. ATAC)
+#' @importFrom SeuratObject CreateSeuratObject
+#' @importFrom Seurat Read10X
+#'
 #' @export
-readCounts10x <- function(filepath, min.cells=100, min.features=10, capID) {
+readCounts10x <- function(filepath,
+                          capID,
+                          min.cells=0,
+                          min.features=0) {
 
   counts_in <- Seurat::Read10X(data.dir = filepath)
-
-  # Accommodate count matrices files with only RNA and those with both RNA and ADT
-  # This is uglier than re-assigning the assay to a new mat obj but saves memory
+  # Accommodate count matrices files with only RNA and those with multiple
   if ("Gene Expression" %in% names(counts_in)) {
-    # Create Seurat obj with no filtering
-    dims0 <- (SeuratObject::CreateSeuratObject(counts_in$`Gene Expression`,
-                                               project = capID,
-                                               min.cells=0, min.features=0))@assays$RNA@counts %>%
-      dim()
-    # Filter min.cells and min.features and report numbers removed
     obj <- SeuratObject::CreateSeuratObject(counts_in$`Gene Expression`,
                                             project = capID,
-                                            min.cells=min.cells, min.features=min.features)
+                                            min.cells = min.cells,
+                                            min.features=min.features)
 
   } else if(is.null(names(counts_in))) {
     print("Only one assay in counts matrix file. Assuming it is Gene Expression.")
-    # Create Seurat obj with no filtering
-    dims0 <- (SeuratObject::CreateSeuratObject(counts_in,
-                                               project = capID,
-                                               min.cells=0, min.features=0))@assays$RNA@counts %>%
-      dim()
-    # Filter min.cells and min.features and report numbers removed
     obj <- SeuratObject::CreateSeuratObject(counts_in,
                                             project = capID,
-                                            min.cells=min.cells, min.features=min.features)
-    print(paste("RNA assay added to Seurat obj with ", length(rownames(obj[["RNA"]])), "features."))
+                                            min.cells = min.cells,
+                                            min.features = min.features)
   } else {
     errorCondition("No Gene Expression assay in counts matrix?")
   }
-
-  ## Check if ADT data are included, add to Seurat obj if so
-  if ("Antibody Capture" %in% names(counts_in)) {
-    # Add ADT UMI matrix
-    obj[["ADT"]] <- SeuratObject::CreateAssayObject(counts = counts_in$`Antibody Capture`)
-    print(paste("ADT assay added to Seurat obj with the following", length(rownames(obj[["ADT"]])), "features:"))
-    print(rownames(obj[["ADT"]]))
-  } else {
-    warning("No Antibody Capture assay found. Proceeding without ADT data.")
-  }
-
   show(obj)
-  obj@misc[["readCounts_nGenesRemoved"]] <- (dims0 - dim(obj@assays$RNA@counts))[1]
-  obj@misc[["readCounts_nCellsRemoved"]] <- (dims0 - dim(obj@assays$RNA@counts))[2]
-  print(paste("Removing", obj@misc[["readCounts_nGenesRemoved"]], "genes and", obj@misc[["readCounts_nCellsRemoved"]], "cells."))
-
   return(obj)
 }
