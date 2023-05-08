d9d61cec0792789c74dccc0e9827d3255817373d
Derrik Gratz <derrik.gratz@gmail.com>
2023-05-08 15:49:01 -0400

    ref to seuratObject package

--- R/readCounts10x.R
+++ R/readCounts10x.R
@@ -4,12 +4,13 @@
 #'
 #' @param filepath path to UMI count matrix (can be raw or unfiltered)
 #' @param capID capture ID label, assigned to 'project' in seurat object metadata
-#' @inheritParams SeuratObject
+#' @param min.cells Include features detected in at least this many cells.
+#'  Will subset the counts matrix as well.
+#'  To reintroduce excluded features, create a new object with a lower cutoff.
 #'
 #' @return An object of class Seurat
 #'
-#' @importFrom SeuratObject CreateSeuratObject
-#' @importFrom Seurat Read10X
+#' @importFrom Seurat Read10X CreateSeuratObject
 #'
 #' @export
 readCounts10x <- function(filepath,
@@ -20,14 +21,14 @@ readCounts10x <- function(filepath,
   counts_in <- Seurat::Read10X(data.dir = filepath)
   # Accommodate count matrices files with only RNA and those with multiple
   if ("Gene Expression" %in% names(counts_in)) {
-    obj <- SeuratObject::CreateSeuratObject(counts_in$`Gene Expression`,
+    obj <- Seurat::CreateSeuratObject(counts_in$`Gene Expression`,
                                             project = capID,
                                             min.cells = min.cells,
                                             min.features=min.features)
 
   } else if(is.null(names(counts_in))) {
     print("Only one assay in counts matrix file. Assuming it is Gene Expression.")
-    obj <- SeuratObject::CreateSeuratObject(counts_in,
+    obj <- Seurat::CreateSeuratObject(counts_in,
                                             project = capID,
                                             min.cells = min.cells,
                                             min.features = min.features)
