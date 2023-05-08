253b878573c04933a965118e7c342147e5eb483b
Derrik Gratz <derrik.gratz@gmail.com>
2023-05-08 15:43:43 -0400

    separate function to remove genes with low presence
    This pairs with updated readCounts10x to allow soupX workflow to slot inbetween reading in data and filtering data, as SoupX needs unfiltered data. This also only applies the gene-level filtering, as the cell level filtering is somewhat redundant with the QCfilter functions

---
+++ R/filterLowGenes.R
@@ -0,0 +1,32 @@
+#' Remove genes present in low number of cells
+#'
+#' Remove rows from Seurat object for genes present in fewer than `min.cells`
+#'  cells. This is normally done in `Seurat::CreateSeuratObject()`, but separating
+#'  this out allows SoupX to be added to the workflow. After SoupX processing,
+#'  this function can be ran to filter the object as you would normally.
+#'
+#' @param obj Seurat object
+#' @param min.cells Minimum number of cells a gene must appear in to be kept
+#' @param calculate_only If `TRUE` only show how many genes would be removed,
+#'  but return the unfiltered object. If `FALSE`, return the filtered object
+#'
+#' @returns An object of class Seurat
+#'
+#' @importFrom Matrix rowSums
+#' @export
+#'
+#' @examples
+filterLowGenes <- function(obj,
+                           min.cells,
+                           calculate_only = TRUE){
+  removed_genes <- (Matrix::rowSums(obj@assays$RNA@counts > 0) < min.cells)
+  message(paste0(obj@project.name, ': ', sum(removed_genes),
+                 ' genes found in fewer than ', min.cells, ' cells'))
+  if (!calculate_only){
+    message('Removing low count genes. New object:')
+    show(obj[!removed_genes, ])
+    return(obj[!removed_genes, ])
+  } else {
+    return(obj)
+  }
+}
