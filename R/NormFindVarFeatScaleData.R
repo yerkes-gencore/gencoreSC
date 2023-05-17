4d60d467719ba847f55606bd70b2762d83069bc4
Micah Fletcher <micahpfletcher@gmail.com>
2023-05-17 15:56:20 -0400

    Finished NormFindVarFeatScaleData().

--- R/NormFindVarFeatScaleData.R
+++ R/NormFindVarFeatScaleData.R
@@ -5,29 +5,28 @@
 #' Each combination of options requires slightly different operations, so this wrapper helps keep code tidy and reproducible.
 #'
 #' @param s Seurat object
-#' @param norm_method Normalization method
-#' @param nfeatures Number of
-regress.out = NULL,
-feature.blacklist = NULL,
-verbose = F,
-scale_data = NULL
+#' @param norm_method Normalization method (either "logNorm" = log transform or "SCT" = SCTransform)
+#' @param nfeatures Number of features to pass to Seurat::FindVariableFeatures()
+#' @param regress.out Features to regress out in SCTransform vars.to.regress
+#' @param feature.blacklist Vector of genes to blacklist from variable features
+#' @param verbose Print all messages from wrapped functions
+#' @param scale_data Logical; whether to run Seurat::ScaleData(). If NULL (default), will scale for log normalization but not for SCTransform.
 #'
-#' @returns An object of class SoupChannel
-#'
-#' @import grDevices
-#' @import SoupX
-#' @importFrom Seurat Read10X
+#' @returns Seurat object
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' soupx_files <- lapply(samples$FileID, function(x){
-#'   findSoupXFiles(file.path(config$rootDir, config$alignmentDir, x))
-#' })
-#' names(soupx_files) <- samples$Label
-#' sc <- lapply(soupx_files, function(x){
-#'   runSoupX(x[['unfiltered_mat_path']], x[['filtered_mat_path']], x[['clusters_path']])
-#' })
+#' # log normalize, find variable features, scale data
+#' s <- NormFindVarFeatScaleData(s, norm_method = "logNorm")
+#'
+#' # SCTransform, find variable features, don't scale data
+#' s <- NormFindVarFeatScaleData(s, norm_method = "SCT")
+#'
+#' # log normalize, find variable features blacklisting TCR gene (Mus musculus), scale data
+#' library(scGate)
+#' blacklist <- scGate::genes.blacklist.default$Mm$TCR
+#' s <- NormFindVarFeatScaleData(s, norm_method = "logNorm", feature.blacklist = blacklist)
 #' }
 NormFindVarFeatScaleData <- function(s,
                                      norm_method = "logNorm",
@@ -48,31 +47,31 @@ NormFindVarFeatScaleData <- function(s,
     feature.blacklist <- unname(unlist(feature.blacklist))
   }
   # I don't think Seurat will try to normalize or scale SCT transformed data but just to be sure:
-  DefaultAssay(s) <- "RNA"
+  Seurat::DefaultAssay(s) <- "RNA"
 
   if (norm_method == 'logNorm'){
-    s <- NormalizeData(s, verbose = verbose)
-    s <- FindVariableFeatures(s,
-                              selection.method = "vst",
-                              nfeatures = nfeatures+length(feature.blacklist),
-                              verbose = verbose)
+    s <- Seurat::NormalizeData(s, verbose = verbose)
+    s <- Seurat::FindVariableFeatures(s,
+                                      selection.method = "vst",
+                                      nfeatures = nfeatures+length(feature.blacklist),
+                                      verbose = verbose)
     # Remove blacklisted features
-    VariableFeatures(s) <- setdiff(VariableFeatures(s), feature.blacklist)
+    Seurat::VariableFeatures(s) <- setdiff(Seurat::VariableFeatures(s), feature.blacklist)
     # Remove extra features
-    VariableFeatures(s) <- VariableFeatures(s)[1:nfeatures]
+    Seurat::VariableFeatures(s) <- Seurat::VariableFeatures(s)[1:nfeatures]
     if (scale_data == T) {
       print("scaling data")
-      s <- ScaleData(s, verbose = verbose)
+      s <- Seurat::ScaleData(s, verbose = verbose)
     }
   } else if (norm_method == 'SCT'){
     features <- setdiff(rownames(s), feature.blacklist)
-    s <- SCTransform(s,
-                     vst.flavor = "v2",
-                     residual.features = features,
-                     variable.features.n = nfeatures,
-                     vars.to.regress = regress.out,
-                     do.scale = scale_data,
-                     verbose = verbose)
+    s <- Seurat::SCTransform(s,
+                             vst.flavor = "v2",
+                             residual.features = features,
+                             variable.features.n = nfeatures,
+                             vars.to.regress = regress.out,
+                             do.scale = scale_data,
+                             verbose = verbose)
   } else if (!is.null(feature.blacklist) & !is.vector(feature.blacklist)) {
     errorCondition("feature.blacklist must be NULL, a vector or a list of vectors of gene symbols.")
   }
