12645d27d81f352563790c288cd32cd7ce341085
Derrik Gratz <derrik.gratz@gmail.com>
2023-07-07 17:11:44 -0400

    refactor qc filtering for per-capture, generalize
    Now doesn't do internal outlier detection, but only uses thresholds. A new function is provided to calculate the thresholds for outlier detection, which can then be passed in to the more genrealized QC filter function.

--- R/addQCfilter.R
+++ R/addQCfilter.R
@@ -3,126 +3,112 @@
 #' Adds logical metadata column for a given set of arbitrary cutoffs and/or outliers
 #'
 #' @param obj Seurat object
-#' @param split.by Metadata column to split by for outlier detection if obj is not already split
 #' @param filterName Name of new metadata column with logical filter
-#' @param by_threshold Logical to control whether to filter on thresholds defined in 'cutoffs'
-#' @param cutoffs Named list of cutoffs
-#' @param by_outlier Logical to control whether to filter outliers
-#' @param nmads Median absolute deviation used to define outliers
+#' @param cutoffs Named list of cutoffs with the following values:
+#'  nUMI.max, nUMI.min, nGene.max, nGene.min, log10GenesPerUMI.max,
+#'  log10GenesPerUMI.min, mitoRatio.max, mitoRatio.min, riboRatio.max, riboRatio.min
+#'   You can provide a single list of values to be used for all captures (not recommended),
+#'   or you can provide a list of lists, where each entry is thresholds for a single capture.
+#'   See `generate_capture_QC_cutoffs()` for help making a cutoffs list.
 #'
 #'
-#' @return Seurat object with new metadata column. If passed a split object, will return split object. Same for a merged/single capture object.
+#' @returns Seurat object(s) with a new metadata column indicating if the cell is within thresholds
+#' @examples
+#' \dontrun{
+#'   ## where objs is a list of Seurat objects for each capture
+#'   cutoffs <- lapply(objs, generate_cutoffs)
+#'   objs <- mapply(addQCfilter, objs, 'outliers', cutoffs)
+#' }
 #'
 #' @export
+
 # calculate metadata filter column but don't filter yet
-addQCfilter <- function(obj, split.by = NULL, filterName = "input",
-                        by_threshold = FALSE, cutoffs = defaultCutoffs,
-                        by_outlier = FALSE, nmads = 4) {
-
-  if (is.list(obj)) {
-    obj <- lapply(obj, FUN = function(x) {
-      x <- addQCfilter.helper(x, filterName = filterName,
-                              by_threshold = by_threshold, cutoffs = cutoffs,
-                              by_outlier = by_outlier, nmads = nmads)
-    })
-  } else if (!is.list(obj)) {
-    if (!by_outlier) {
-      obj <- addQCfilter.helper(obj, filterName = filterName,
-                                by_threshold = by_threshold, cutoffs = cutoffs,
-                                by_outlier = by_outlier, nmads = nmads)
-    } else if (is.null(split.by)) {
-      # split the object
-      obj <- Seurat::SplitObject(obj, split.by = split.by)
-      # lapply over split object with addQCfilter.helper
-      obj <- lapply(obj, FUN = function(x) {
-        x <- addQCfilter.helper(x, filterName = filterName,
-                                by_threshold = by_threshold, cutoffs = cutoffs,
-                                by_outlier = by_outlier, nmads = nmads)
-      })
-      obj <- merge(x=obj[[1]], y=obj[2:length(obj)])
-    } else if (!is.null(split.by)) {
-      warning("Outlier detection on a merged object is not recommended. Either split object or specify split.by param.")
-    }
+addQCfilter <- function(obj,
+                        filterName = NULL,
+                        cutoffs = NULL) {
+  if (is.null(filterName)){
+    stop('Use the "filterName" argument to specify a name/metadata column to store filtering outcomes')
   }
-    return(obj)
+  if (is.null(cutoffs)){
+    message('No cutoffs provided. Consider running `generate_capture_QC_cutoffs()` to tailor cutoffs to your captures, or
+            manually provide cutoffs.\nUsing arbitrary default values to filter which may not be appropriate for your dataset.')
+    cutoffs <- list(nUMI.max = Inf,
+                    nUMI.min = 500,
+                    nGene.max = Inf,
+                    nGene.min = 250,
+                    log10GenesPerUMI.max = Inf,
+                    log10GenesPerUMI.min = 0.80,
+                    mitoRatio.max = 0.2,
+                    mitoRatio.min = 0,
+                    riboRatio.max = 0.4,
+                    riboRatio.min = 0)
   }
+  ## For ridges plot, need a bool to filter cells on
+  obj@meta.data$input <- TRUE
 
-addQCfilter.helper <- function(obj, filterName = "input",
-                               by_threshold = FALSE, cutoffs = defaultCutoffs,
-                               by_outlier = FALSE, nmads = 4) {
-
-    print(paste0("Creating new metadata column to store filter logical: ", filterName))
-
-    # Create new metadata column with cell-level filtering logical
-    if(by_threshold) {
-      metadata <- obj@meta.data
-      metadata <- metadata %>% dplyr::mutate(
-        cutoffsOnly =
-          .data$nUMI >= cutoffs$nUMI.min &
-          .data$nUMI <= cutoffs$nUMI.max &
-          .data$nGene >= cutoffs$nGene.min &
-          .data$nGene <= cutoffs$nGene.max &
-          .data$log10GenesPerUMI >= cutoffs$log10GenesPerUMI.min &
-          .data$log10GenesPerUMI <= cutoffs$log10GenesPerUMI.max &
-          .data$mitoRatio >= cutoffs$mitoRatio.min &
-          .data$mitoRatio <= cutoffs$mitoRatio.max &
-          .data$riboRatio >= cutoffs$riboRatio.min &
-          .data$riboRatio <= cutoffs$riboRatio.max)
-      obj@meta.data <- metadata
-    } else {
-      metadata <- obj@meta.data
-      metadata <- metadata %>%
-        dplyr::mutate(cutoffsOnly = TRUE)
-      obj@meta.data <- metadata
-    }
-    # Create outlier metadata column if not already present
-    if(! "outliers" %in% colnames(obj@meta.data)) {
-      obj <- detect_outliers(obj, nmads)
-    }
-    # Create final filtering logical column based on whether filtering by outlier, cutoffs or both
-    if (!by_outlier) {
-      metadata <- obj@meta.data
-      metadata <- metadata %>%
-        dplyr::mutate(!!filterName := .data$cutoffsOnly) %>%
-        dplyr::select(-.data$cutoffsOnly)
-      obj@meta.data <- metadata
-    } else {
-      metadata <- obj@meta.data
-      metadata <- metadata %>%
-        # set logical to match only cells that pass the cutoffs AND are not outliers
-        dplyr::mutate(!!filterName := .data$cutoffsOnly & !.data$outlier) #%>%
-      obj@meta.data <- metadata
-    }
+  ## filtering
+  metadata <- obj@meta.data
+  metadata <- metadata %>% dplyr::mutate(
+    QC_filtered =
+      .data$nUMI >= cutoffs$nUMI.min &
+      .data$nUMI <= cutoffs$nUMI.max &
+      .data$nGene >= cutoffs$nGene.min &
+      .data$nGene <= cutoffs$nGene.max &
+      .data$log10GenesPerUMI >= cutoffs$log10GenesPerUMI.min &
+      .data$log10GenesPerUMI <= cutoffs$log10GenesPerUMI.max &
+      .data$mitoRatio >= cutoffs$mitoRatio.min &
+      .data$mitoRatio <= cutoffs$mitoRatio.max &
+      .data$riboRatio >= cutoffs$riboRatio.min &
+      .data$riboRatio <= cutoffs$riboRatio.max)
+  obj@meta.data <- metadata
   return(obj)
 }
 
-# Cell-level metrics
-defaultCutoffs <- list(nUMI.max = Inf,
-                       nUMI.min = 500,
-                       nGene.max = Inf,
-                       nGene.min = 250,
-                       log10GenesPerUMI.max = Inf,
-                       log10GenesPerUMI.min = 0.80,
-                       mitoRatio.max = 0.2,
-                       mitoRatio.min = 0,
-                       riboRatio.max = 0.4,
-                       riboRatio.min = 0)
+#' generate_capture_QC_cutoffs
+#'
+#' Generates default cutoffs for all metrics of interest based on median
+#' absolute deviation for the distribution of metric values in the capture.
+#'
+#' @param capture A seurat object
+#' @param nmads number of median absolute deviations, default 4
+#' @param metrics Columns of the capture metadata to calculate thresholds for
+#'
+#' @returns a list of min and max thresholds for each of `metrics`
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   cutoffs <- generate_cutoffs(objs$Apr05_HH_5A)
+#'   ## multiple captures
+#'   cutoffs <- lapply(objs, generate_cutoffs)
+#' }
+generate_capture_QC_cutoffs <- function(capture,
+                                        nmads = 4,
+                                        metrics = c('nUMI',
+                                                    'nGene',
+                                                    'riboRatio',
+                                                    'mitoRatio',
+                                                    'log10GenesPerUMI')){
+  cutoffs <- list()
+  for (metric in metrics) {
+    thresholds <- .generate_outlier_thresholds(capture, metric, nmads)
+    cutoffs[[paste0(metric, '.max')]] <- thresholds[['upper']]
+    cutoffs[[paste0(metric, '.min')]] <- thresholds[['lower']]
+  }
+  return(cutoffs)
+}
 
-### Outlier detection for a vector of values, based on median absolute deviation
-is_outlier_mad <- function(data, nmads){
+## Generates upper and lower threshold based on MAD calculation
+## helper function for generate_capture_QC_cutoffs
+.generate_outlier_thresholds <- function(capture,
+                                         metric,
+                                         nmads,
+                                         min.value = 0){
+  data <- capture@meta.data[[metric]]
   data.mad <- stats::mad(data)
-  outlier = (
-    (data < stats::median(data) - nmads * data.mad) |
-      (stats::median(data) + nmads * data.mad < data)
-  )
-  return(outlier)
-}
-
-detect_outliers <- function(obj, nmads=4){
-  ## Log transforming data to normalize it
-  outliers <- (is_outlier_mad(log1p(obj$nUMI), nmads) |
-                   is_outlier_mad(log1p(obj$nGene), nmads) |
-                   is_outlier_mad(log1p(obj$log10GenesPerUMI), nmads))
-  obj$outlier <- outliers
-  return(obj)
+  data.med <- stats::median(data)
+  upper <- data.med + nmads * data.mad
+  lower <- data.med - nmads * data.mad
+  if (lower < min.value) { lower <- min.value }
+  return(list(upper = upper, lower = lower))
 }
