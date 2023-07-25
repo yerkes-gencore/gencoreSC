5647d0dc020654700fc4358f82b33a2ba39948e2
micahpf <micahpfletcher@gmail.com>
2023-07-25 16:42:39 -0400

    renamed `checkPCA()` to `plotPCAs()` and move from `scoreCC.R` to its own file; see issue #16 and #59

--- R/scoreCC.R
+++ R/scoreCC.R
@@ -73,23 +73,3 @@ scoreCC <- function(obj, s.genes, g2m.genes) {
 
   return(obj.phase)
 }
-
-#' Plot PCAs for a list of categorical features
-#'
-#' Take a vector of features and plot PCA plots for each, returning a list.
-#'
-#' @param obj Seurat object
-#' @param features Vector of features to to group.by and split.by in PCA plots
-#'
-#' @return List of plot objects
-#' @export
-checkPCA <- function(obj, features = c("Phase", "mitoRatio", "riboRatio")) {
-  p.list <- list()
-  for (feature in features) {
-    p.list[[feature]] <- Seurat::DimPlot(obj,
-                               reduction = "pca",
-                               group.by= feature,
-                               split.by = feature)
-  }
-  return(p.list)
-}
